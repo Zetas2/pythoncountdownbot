be3446a30b6f60991e11405e899f3d2a92cc26b5
Zetas2 <43906711+Zetas2@users.noreply.github.com>
2022-09-09 08:51:34 +0200

    Made title be title

--- languageFile.py
+++ languageFile.py
@@ -1,10 +1,8 @@
 translations = {
     "en-US": {
-        "helpTitle": "Help",
-        "helpHeader": "/Help",
+        "helpHeader": "Help",
         "helpHover": "To see when the timer run out, you can hover/click on the timestamp.",
         "helpDesc": "This bot got the following commands:",
-
         "helpLeftTitle": "/Timeleft",
         "helpLeftDesc": "Shows the exact time left for a countdown.",
         "helpCountdownTitle": "/Countdown",
@@ -13,6 +11,7 @@ translations = {
         "helpListDesc": "It will show you all active countdowns in the channel/guild or from you depending on subcommand.",
         "helpDeleteTitle": "/Delete",
         "helpDeleteDesc": "To use this with other than *mine* you need to have the `MANAGE_MESSAGE` permission.\n*Single*\nPick one of the list presented to you. To get more info about what countdown it is, use /list.\n*Mine*\nDelete the countdown you created.\n*Channel*\nDeletes all countdowns in this channel.\n*Guild*\nDeletes all countdowns in this guild.",
+        "helpTitle": "/Help",
         "helpHelpDesc": "Shows this help message",
         "helpLinksTitle": "/Links",
         "helpLinksDesc": "[Discord Support](https://discord.com/invite/b2fY4z4xBY 'Join the support guild!') | [Invite the Bot](https://top.gg/bot/710486805836988507) | [Patreon](https://www.patreon.com/livecountdownbot)",
