a7f81a578cb5b40f70874742571eec8148596961
0xGingi <104647854+0xGingi@users.noreply.github.com>
2023-09-13 13:02:01 -0400

    Update bot.py (#43)
    * Update bot.py
    
    * removed a comment that shouldnt be there
    
    Thanks 0xGingi
    
    ---------
    
    Co-authored-by: Zetas2 <43906711+Zetas2@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -867,7 +867,6 @@ async def listpremium(ctx: interactions.SlashContext, page=1):
 async def latencies(ctx: interactions.SlashContext):
     result = bot.latencies
     await ctx.send(
-    some-dev-commands
         f"{result}"
     )
     
